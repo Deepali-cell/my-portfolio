1769fd3dc4afe001be5b5cc7e001334fb23eace9
Deepali <joysingal247@gmail.com>
2025-08-29 21:52:09 +0530

    enable the image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export" };
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true, // disables the Image Optimization API
+  },
+};
+
+module.exports = nextConfig;
 
 export default nextConfig;
